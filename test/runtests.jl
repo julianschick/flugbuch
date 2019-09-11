489b2e9931cbe8d1ffb4b8950a8a4cd44605a7b8
Julian Schick <julian.schick@posteo.de>
2019-09-12 01:19:54 +0200

    fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,19 @@ import Flugbuch
 
 using Dates
 
+exampleDir = @__DIR__
+
 # in case last tear down did not work
-if isfile("runtest.db")
-    rum("runtest.db")
+if isfile(joinpath(exampleDir, "runtest.db"))
+    rm(joinpath(exampleDir, "runtest.db"))
 end
 
-Flugbuch.loadConfig("examples/flugbuchrc")
-Flugbuch.create("runtest.db")
-Flugbuch.csv("examples/import.csv", autoconfirm = true)
+Flugbuch.loadConfig(joinpath(exampleDir, "examples", "flugbuchrc"))
+Flugbuch.create(joinpath(exampleDir, "runtest.db"))
+Flugbuch.csv(joinpath(exampleDir, "examples", "import.csv"), autoconfirm = true)
 Flugbuch.prt()
 Flugbuch.prt(beginNr=2, endNr=2)
 Flugbuch.prt(beginDate=Date(2018,1,1), endDate=Date(2019,1,1))
 Flugbuch.unload()
 
-rm("runtest.db")
+rm(joinpath(exampleDir, "runtest.db"))
